689ef477b1ef129fdbdbb1a069734899e3c25324
LucaBorzone13 <92693790+LucaBorzone13@users.noreply.github.com>
2021-11-14 17:23:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
-using namespace std;
+
+int fibo(int n){
+
+    int i;
+    int a=1, b=1, c;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+
+    for(i=2; i<n; i++){
+
+        c=a+b;
+
+        std::cout << c << std::endl;
+
+        a=b;
+        b=c;
+
+
+    }
+    
+}
+
 
 int main() {
-   return 0;
+
+    int n=0;
+    std::cin >> n;
+
+
+    if(n<2){
+
+        std::cout << "errore" << std::endl;
+
+    }else if (n>=2){
+
+        int x = fibo(n);
+        std::cout << x << std::endl;
+
+    }
+
+    return 0;
 }
